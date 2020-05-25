0d2c1548b69110ecb115ea076f117220199a0640
Claudio Gomes <claudiocreates@gmail.com>
2020-05-25 08:24:56 -0300

    Screenshot não tira em 5 sec

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,5 +1,5 @@
-setTimeout(function() {
+function fotor() {
     html2canvas(document.getElementById('tela')).then(function(canvas) {
         document.body.appendChild(canvas);
     });
-},5000);
+};
